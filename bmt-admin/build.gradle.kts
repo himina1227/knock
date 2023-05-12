1ae235c0fb81310697b6f899ee355f23fe9ae851
nakgyeom <himina1227@gmail.com>
2023-05-12 09:40:06 +0900

    - bmt-common 모듈 추가 - spring-security 모듈 추가

--- bmt-admin/build.gradle.kts
+++ bmt-admin/build.gradle.kts
@@ -27,10 +27,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation(project(":bmt-common"))
 }
 
 tasks.withType<KotlinCompile> {
