53a0cdd36cff5adec6e5d26b17b79ab17f83ec61
himina1227 <himina1227@gmail.com>
2023-05-21 23:44:41 +0900

    - member-admin: 컨트롤러 수정

--- bmt-admin/src/main/kotlin/com/knock/bmt/admin/aggregate/member/adapter/in/rest/CommonController.kt
+++ bmt-admin/src/main/kotlin/com/knock/bmt/admin/aggregate/member/adapter/in/rest/CommonController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/aa")
+@RequestMapping
 class CommonController(val configServerConfig: ConfigServerConfig) {
     @GetMapping("/index")
     fun hello(): String? {
